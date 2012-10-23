4dda01554c97cdcc3a00d19e04ef8df0a72ed744
Ahsan Javed <ahsan_cse2004@yahoo.com>
2012-10-23 15:25:50 +0530

    Initial checkin - Ahsan

--- androidapp/src/com/webshrub/moonwalker/androidapp/HomeActivity.java
+++ androidapp/src/com/webshrub/moonwalker/androidapp/HomeActivity.java
@@ -3,12 +3,12 @@ package com.webshrub.moonwalker.androidapp;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class HomeActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class HomeActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }

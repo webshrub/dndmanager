623762437fec3bef74f701b855dbc6559de033c3
unknown <WS1@.(none)>
2012-10-31 14:45:11 +0530

    Added Functionality to register/unregister for dnd - Ankur

--- androidapp/src/com/webshrub/moonwalker/androidapp/HomeActivity.java
+++ androidapp/src/com/webshrub/moonwalker/androidapp/HomeActivity.java
@@ -7,7 +7,12 @@ public class HomeActivity extends DroidGap {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+//        Toast.makeText(getApplicationContext(), "Starting Application", Toast.LENGTH_SHORT).show();
+
         super.loadUrl("file:///android_asset/www/index.html");
+//        Toast.makeText(getApplicationContext(), "Application started.", Toast.LENGTH_SHORT).show();
+
     }
 }

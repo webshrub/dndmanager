1e17d2758b41c1594a734bee11dc157afc194f92
Ahsan Javed <ahsan_cse2004@yahoo.com>
2012-11-01 11:06:38 +0530

    Initial checkin - Ahsan

--- androidapp/src/com/webshrub/moonwalker/androidapp/SmsPlugin.java
+++ androidapp/src/com/webshrub/moonwalker/androidapp/SmsPlugin.java
@@ -30,14 +30,12 @@ import org.apache.cordova.api.PluginResult.Status;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-
 public class SmsPlugin extends Plugin {
     public final String ACTION_SEND_SMS = "SendSMS";
 
     @Override
     public PluginResult execute(String action, JSONArray arg1, String callbackId) {
         PluginResult result = new PluginResult(Status.INVALID_ACTION);
-
         if (action.equals(ACTION_SEND_SMS)) {
             try {
                 String phoneNumber = arg1.getString(0);
@@ -48,16 +46,12 @@ public class SmsPlugin extends Plugin {
                 result = new PluginResult(Status.JSON_EXCEPTION, ex.getMessage());
             }
         }
-
         return result;
     }
 
     private void sendSMS(String phoneNumber, String message) {
         SmsManager manager = SmsManager.getDefault();
-
         PendingIntent sentIntent = PendingIntent.getActivity(this.ctx.getContext(), 0, new Intent(), 0);
-
         manager.sendTextMessage(phoneNumber, null, message, sentIntent, null);
     }
-
 }

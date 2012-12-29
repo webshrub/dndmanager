31ab03abc7f2bac2f467fec685afa244d4bedb42
Ahsan Javed <ahsan_cse2004@yahoo.com>
2012-12-29 12:40:32 +0530

    Renamed id variables with meaningful names - Ahsan

--- androidapp/assets/www/settings.js
+++ androidapp/assets/www/settings.js
@@ -1,22 +1,22 @@
 createSpinner("res/lib/jquerymobile/images/ajax-loader.gif");
 
 $("#settings").die("pageinit").live("pageinit", function (event, ui) {
-    var value = moonwalkerStorage.getItem("deleteSMSFlag");
-    $("#j_47").val(value).slider("refresh");
-    $("#j_47").change(function (event, ui) {
+    var deleteSMSFlag = moonwalkerStorage.getItem("deleteSMSFlag");
+    $("#deleteSmsToggleButton").val(deleteSMSFlag).slider("refresh");
+    $("#deleteSmsToggleButton").change(function (event, ui) {
         moonwalkerStorage.setItem("deleteSMSFlag", $(this).val());
     });
 
 
-    var value1 = moonwalkerStorage.getItem("saveMessageFlag");
-    $("#j_50").val(value1).slider("refresh");
-    $("#j_50").change(function (event, ui) {
+    var saveSMSFlag = moonwalkerStorage.getItem("saveMessageFlag");
+    $("#saveMessageToggleButton").val(saveSMSFlag).slider("refresh");
+    $("#saveMessageToggleButton").change(function (event, ui) {
         moonwalkerStorage.setItem("saveMessageFlag", $(this).val());
     });
 
-    var value2 = moonwalkerStorage.getItem("helpFlag");
-    $("#j_54").val(value2).slider("refresh");
-    $("#j_54").change(function (event, ui) {
-        moonwalkerStorage.setItem("helpFlag", $(this).val());
+    var analyticsFlag = moonwalkerStorage.getItem("analyticsFlag");
+    $("#analyticsToggleButton").val(analyticsFlag).slider("refresh");
+    $("#analyticsToggleButton").change(function (event, ui) {
+        moonwalkerStorage.setItem("analyticsFlag", $(this).val());
     });
 });

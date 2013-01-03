ff37f1c7d588aba1d6aa69151968d7a66d8c85d6
Ahsan Javed <ahsan_cse2004@yahoo.com>
2013-01-03 22:38:51 +0530

    fixed sms sending code - Ahsan

--- androidapp/assets/www/js/plugin/smsplugin.js
+++ androidapp/assets/www/js/plugin/smsplugin.js
@@ -3,5 +3,5 @@ var SmsPlugin = function () {
 
 SmsPlugin.prototype.send = function (phone, message, reportType, isToDelete, number, successCallback, failureCallback) {
 //    alert("Sending " + message + " to " + phone);
-    return cordova.exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", ["phone", message, reportType, isToDelete, number]);
+    return cordova.exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", [phone, message, reportType, isToDelete, number]);
 };

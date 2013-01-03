4ff34691bee900303369b06b36b02b662e35d11d
Ankur <ankur.agarwal@webshrub.com>
2013-01-03 16:30:07 +0530

    Updated smsplugin.js - Ankur

--- androidapp/assets/www/js/plugin/smsplugin.js
+++ androidapp/assets/www/js/plugin/smsplugin.js
@@ -1,7 +1,7 @@
 var SmsPlugin = function () {
 };
 
-SmsPlugin.prototype.send = function (phone, message, successCallback, failureCallback) {
+SmsPlugin.prototype.send = function (phone, message, reportType, isToDelete, number, successCallback, failureCallback) {
 //    alert("Sending " + message + " to " + phone);
-    return cordova.exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", [phone, message]);
+    return cordova.exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", ["phone", message, reportType, isToDelete, number]);
 };

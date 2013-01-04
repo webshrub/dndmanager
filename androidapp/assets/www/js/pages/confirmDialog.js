cd2158ed2dbaa40dea471bb1d74b7f49a98aad63
Ankur <ankur.agarwal@webshrub.com>
2013-01-04 12:41:42 +0530

    Implemented saving sent message setting for report all  - Ankur

--- androidapp/assets/www/js/pages/confirmDialog.js
+++ androidapp/assets/www/js/pages/confirmDialog.js
@@ -9,9 +9,11 @@ $("#confirmDialog").die("pageinit").live("pageinit", function (event, ui) {
         var deleteSMSFlag = moonwalkerStorage.getItem("deleteSMSFlag");
         var reportType = moonwalkerStorage.getItem("reportType");
         var smsTextObjects = moonwalkerStorage.getItem("smsTextObjects");
+        var saveSMSFlag = moonwalkerStorage.getItem("saveMessageFlag");
+
         for (var counter = 0; counter < smsTextObjects.length; counter++) {
             var smsText = "COMP TEL NO " + smsTextObjects[counter].number + ";" + smsTextObjects[counter].date + ";" + smsTextObjects[counter].text;
-            new SmsPlugin().send('1909', smsText, reportType, deleteSMSFlag, smsTextObjects[counter].number,
+            new SmsPlugin().send('1909', smsText, reportType, deleteSMSFlag, saveSMSFlag, smsTextObjects[counter].number,
                 function () {
 //                    alert('Message sent successfully');
                 },
--- androidapp/assets/www/js/plugin/smsplugin.js
+++ androidapp/assets/www/js/plugin/smsplugin.js
@@ -1,7 +1,7 @@
 var SmsPlugin = function () {
 };
 
-SmsPlugin.prototype.send = function (phone, message, reportType, isToDelete, saveSMSFlag, number, successCallback, failureCallback) {
+SmsPlugin.prototype.send = function (phone, message, reportType, deleteSMSFlag, saveSMSFlag, number, successCallback, failureCallback) {
 //    alert("Sending " + message + " to " + phone);
-    return cordova.exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", [phone, message, reportType, isToDelete, saveSMSFlag, number]);
+    return cordova.exec(successCallback, failureCallback, 'SmsPlugin', "SendSMS", [phone, message, reportType, deleteSMSFlag, saveSMSFlag, number]);
 };

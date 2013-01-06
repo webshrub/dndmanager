4e0a115613102af0b93011a241d15c5fb3f0bddb
Ahsan Javed <ahsan_cse2004@yahoo.com>
2013-01-06 18:25:34 +0530

    implemented multi page html to support status message - Ahsan

--- androidapp/assets/www/js/pages/confirmDialog.js
+++ androidapp/assets/www/js/pages/confirmDialog.js
@@ -12,17 +12,19 @@ $("#confirmDialog").die("pageinit").live("pageinit", function (event, ui) {
         var smsTextObjects = moonwalkerStorage.getItem("smsTextObjects");
         var saveSMSFlag = moonwalkerStorage.getItem("saveMessageFlag");
 
+        var totalCounter = smsTextObjects.length;
+        var closeDialog = function () {
+            totalCounter -= 1;
+            if (totalCounter <= 0) {
+                $.mobile.hidePageLoadingMsg();
+                $.mobile.showPageLoadingMsg("b", "Messages sent.", true);
+                $('#confirmDialog').dialog('close');
+            }
+        };
         for (var counter = 0; counter < smsTextObjects.length; counter++) {
             var smsText = "COMP TEL NO " + smsTextObjects[counter].number + ";" + smsTextObjects[counter].date + ";" + smsTextObjects[counter].text;
             new SmsPlugin().send('1909', smsText, reportType, deleteSMSFlag, saveSMSFlag, smsTextObjects[counter].number,
-                function () {
-                    if (counter == smsTextObjects.length) {
-                        $.mobile.hidePageLoadingMsg();
-                        $.mobile.showPageLoadingMsg("b", "Messages sent.", true);
-                        $('#confirmDialog').dialog('close');
-                    }
-                },
-                function (e) {
+                closeDialog, function (e) {
                     alert('Message Failed:' + e);
                 }
             );

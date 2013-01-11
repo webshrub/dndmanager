f38f878af944b4065beeb17786fa3e27d776f167
Ahsan Javed <ahsan_cse2004@yahoo.com>
2013-01-11 15:04:19 +0530

    Deleting unused resources - Ahsan

--- androidapp/assets/www/js/pages/splash.js
+++
@@ -1,5 +0,0 @@
-//createSpinner("res/lib/jquerymobile/images/ajax-loader.gif");
-
-$("#splash").die("pageshow").live("pageshow", function (event, ui) {
-    $.mobile.changePage($('#category'));
-});

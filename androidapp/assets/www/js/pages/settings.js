461ac69b4b1867ea7f52cf8e6dde0c247ab47379
Ankur <ankur.agarwal@webshrub.com>
2013-01-11 12:27:46 +0530

    Fixed initial values of Settings - Ankur

--- androidapp/assets/www/js/pages/settings.js
+++ androidapp/assets/www/js/pages/settings.js
@@ -1,22 +1,31 @@
 //createSpinner("res/lib/jquerymobile/images/ajax-loader.gif");
 
+$("#settings").die("pageshow").live("pageshow", function (event, ui) {
+    var deleteSMSFlag = moonwalkerStorage.getItem("deleteSMSFlag");
+    $("#deleteSmsToggleButton").val(deleteSMSFlag).slider("refresh");
+
+    var deleteSentSMSFlag = moonwalkerStorage.getItem("deleteSentSMSFlag");
+    $("#deleteSentSMSToggleButton").val(deleteSentSMSFlag).slider("refresh");
+});
+
 $("#settings").die("pageinit").live("pageinit", function (event, ui) {
     var deleteSMSFlag = moonwalkerStorage.getItem("deleteSMSFlag");
+    if (deleteSMSFlag == null) {
+        deleteSMSFlag = "off";
+        moonwalkerStorage.setItem("deleteSMSFlag", deleteSMSFlag);
+    }
     $("#deleteSmsToggleButton").val(deleteSMSFlag).slider("refresh");
     $("#deleteSmsToggleButton").change(function (event, ui) {
         moonwalkerStorage.setItem("deleteSMSFlag", $(this).val());
     });
 
-
     var deleteSentSMSFlag = moonwalkerStorage.getItem("deleteSentSMSFlag");
+    if (deleteSentSMSFlag == null) {
+        deleteSentSMSFlag = "off";
+        moonwalkerStorage.setItem("deleteSentSMSFlag", deleteSentSMSFlag);
+    }
     $("#deleteSentSMSToggleButton").val(deleteSentSMSFlag).slider("refresh");
     $("#deleteSentSMSToggleButton").change(function (event, ui) {
         moonwalkerStorage.setItem("deleteSentSMSFlag", $(this).val());
-    });
-
-    var analyticsFlag = moonwalkerStorage.getItem("analyticsFlag");
-    $("#analyticsToggleButton").val(analyticsFlag).slider("refresh");
-    $("#analyticsToggleButton").change(function (event, ui) {
-        moonwalkerStorage.setItem("analyticsFlag", $(this).val());
     });
 });

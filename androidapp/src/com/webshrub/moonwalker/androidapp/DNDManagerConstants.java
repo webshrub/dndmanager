035a5f246198eea6d73ede62a353bd4e0be6f464
Ahsan Javed <ahsan_cse2004@yahoo.com>
2013-07-26 13:51:02 +0530

    Refactoring to remove unnecessary code from html5 pages  - Ahsan

--- androidapp/src/com/webshrub/moonwalker/androidapp/DNDManagerConstants.java
+++ androidapp/src/com/webshrub/moonwalker/androidapp/DNDManagerConstants.java
@@ -6,7 +6,6 @@ public class DNDManagerConstants {
     public static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("dd/MM/yy");
     public static final SimpleDateFormat SIMPLE_DATE_TIME_FORMAT = new SimpleDateFormat("dd/MM/yy;kk:mm");
     public static final String TRAI_CONTACT_NUMBER = "1909";
-    public static final String ID_COLUMN = "_id";
     public static final String ADDRESS_COLUMN = "address";
     public static final String DATE_COLUMN = "date";
     public static final String BODY_COLUMN = "body";

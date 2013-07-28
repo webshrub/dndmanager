fa2fec21d9657eace7fcd7c77faa220634a334e5
Ahsan Javed <ahsan_cse2004@yahoo.com>
2013-07-28 14:06:44 +0530

    No schema upgrade  - Ahsan

--- androidapp/src/com/webshrub/moonwalker/androidapp/DNDManagerSQLiteHelper.java
+++ androidapp/src/com/webshrub/moonwalker/androidapp/DNDManagerSQLiteHelper.java
@@ -7,6 +7,7 @@ import android.util.Log;
 
 public class DNDManagerSQLiteHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "dnd_manager.db";
+    public static final int DATABASE_VERSION = 1;
     public static final String TABLE_IGNORED_CONTACT = "ignored_contact";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_NUMBER = "number";
@@ -25,7 +26,7 @@ public class DNDManagerSQLiteHelper extends SQLiteOpenHelper {
             + ");";
 
     public DNDManagerSQLiteHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
@@ -36,7 +37,5 @@ public class DNDManagerSQLiteHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         Log.w(DNDManagerSQLiteHelper.class.getName(), "Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_IGNORED_CONTACT);
-        onCreate(db);
     }
 }
